5fbf6162cd7d9f991aa2ed845d998d056a773773
ParaPup <mk.rules1@gmail.com>
2018-04-09 19:01:26 +1000

    First commit
    The whole game!

---
+++ AwesomePlatformer/Player.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AwesomePlatformer
+{
+    class Player
+    {
+        Sprite sprite = new Sprite();
+
+        public Player()
+        {
+        }
+
+        public void Load(ContentManager content)
+        {
+            sprite.Load(content, "adventurer_stand");
+        }
+
+        public void Update(float deltaTime)
+        {
+            sprite.Update(deltaTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch);
+        }
+    }
+}
